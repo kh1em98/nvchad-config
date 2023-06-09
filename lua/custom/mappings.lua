93ca470430bbd184884fecbb16511a2c210d4af9
Felix <khiem.na@carehealth.io>
2023-06-09 16:52:47 +0700

    feat: upload custom

---
+++ lua/custom/mappings.lua
@@ -0,0 +1,14 @@
+local M = {}
+
+M.general = {
+  n = {
+    [";"] = { ":", "enter command mode", opts = { nowait = true }},
+    ["j"] = {"jzz", "move down and center"},
+    ["k"] = {"kzz", "move up and center"},
+    ["J"] = {"5jzz", "move down faster"},
+    ["K"] = {"5kzz", "move up faster"},
+    ["<C-j>"] = {"i<CR><ESC>" ,"New line"},
+  }
+}
+
+return M;
